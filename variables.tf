d5b734398f6ada0715121fbe2eb655e059fd14a7
Eric Vincent Fomo <evfomo@gmail.com>
2024-02-22 19:43:30 -0600

    vpc-module

---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "availability_zone" {
+  type = list(any)
+  default = [
+    "us-east-1a",
+    "us-east-1b",
+    "us-east-1c",
+  ]
+}
+
+variable "tags" {
+  type = map(any)
+  default = {
+    "id"             = "2024"
+    "owner"          = "vincent DevOps"
+    "teams"          = "PD"
+    "environment"    = "dev"
+    "project"        = "VPC"
+    "create_by"      = "Terraform"
+    "cloud_provider" = "aws"
+  }
+}

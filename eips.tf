446238c70bddcdf889e28bfec03e9410180032ad
Eric Vincent Fomo <evfomo@gmail.com>
2024-02-24 14:15:50 -0600

    changes

--- eips.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_eip" "vincent_eip" {
-  count = length(var.availability_zone)
-  vpc   = true
-
-  tags = merge(var.tags, {
-    Name = format("%s-%s-%s-eip-%d-%s", var.tags["id"], var.tags["environment"], var.tags["project"], count.index + 1, element(var.availability_zone, count.index))
-  })
-  depends_on = [aws_internet_gateway.vincent_igw]
-}
--- nat.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_nat_gateway" "vincent_nat_gateway" {
-  count         = length(var.availability_zone)
-  subnet_id     = aws_subnet.vincent_public_subnet[count.index].id
-  allocation_id = aws_eip.vincent_eip[count.index].id
-
-  tags = merge(var.tags, {
-    Name = format("%s-%s-%s-nat-%d-%s", var.tags["id"], var.tags["environment"], var.tags["project"], count.index + 1, element(var.availability_zone, count.index))
-  })
-}
